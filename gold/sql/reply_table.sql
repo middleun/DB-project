12a457c3fd783ba5ce728efe59b8dd5c2360b079
middleun <eunallaco@gmail.com>
2021-02-04 12:31:30 +0900

    210204_3차 호스팅 후 push / db Project 1차 베타 퍼블리싱

--- gold/sql/reply_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `gold_re` (
-  `GOLD_re_num` int(11) NOT NULL COMMENT '고유변호',
-  `GOLD_re_wr_num` int(11) NOT NULL COMMENT '원글 번호',
-  `GOLD_re_id` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '답글 작성자',
-  `GOLD_re_cont` text NOT NULL COMMENT '답글 내용'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
--- gold/sql/wirte_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `gold_wr` (
-`GOLD_wr_num` int(11) NOT NULL COMMENT '고유번호',
-`GOLD_wr_id` varchar(30) CHARACTER SET utf8 NOT NULL COMMENT '글쓰기 아이디',
-`GOLD_wr_title` varchar(50) CHARACTER SET utf8 NOT NULL COMMENT '글쓰기 제목',
-`GOLD_wr_cont` text CHARACTER SET utf8 NOT NULL COMMENT '글쓰기 내용',
-`GOLD_wr_hit` int(11) NOT NULL COMMENT '조회수'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
