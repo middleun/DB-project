8391869cee0d69575d99c185ad437841741daa8e
middleun <eunallaco@gmail.com>
2021-03-23 18:15:24 +0900

    210323_redesign 작업중

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Gold</title>
+    <title>Greenery</title>
 
     <!-- favicon link -->
     <link rel="icon" href="/gold/img/favicon.ico"/>
